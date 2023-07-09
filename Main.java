3c3b0dfcf6fb7f2d3b94dc92a0ebb2dceb3cfba9
AlexBatista55 <104907236+AlexBatista55@users.noreply.github.com>
2023-07-09 18:56:24 -0300

    Atualizacao semana 2
    Inclusão do Editar nome
    Inclusão de sexo no cadastro pessoa
    Calculo Idade Pessoas
    Calculo pessoas por Sexo

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
@@ -16,12 +16,11 @@ public class Main {
             System.out.println("1 - Cadastrar uma pessoa");
             System.out.println("2 - Buscar uma pessoa cadastrada");
             System.out.println("3 - Excluir uma pessoa cadastrada ");
-            System.out.println("4 - Sair do sistema ");
+            System.out.println("4 - Editar o nome de uma pessoa cadastrada ");
+            System.out.println("5 - Idade das pessoas cadastradas");
+            System.out.println("6 - Sexo pessoas cadastradas");
+            System.out.println("7 - Sair do sistema ");
             int option = scanner.nextInt();
-            //  aqui tentei colocar uma validação, caso o usuario nao digitasse um numero inteiro, desse alguma mensagem
-            // encontrei alguns metodos que não deram certo:
-            // *(if (!(option instanceof Integer)))*
-            // *(if (option.getClass() != int.class && option.getClass() != Integer.class)))
             switch (option){
 
                 case 1 :
@@ -38,6 +37,22 @@ public class Main {
                     removePessoa(pessoas, nameToRemove);
                     break;
                 case 4 :
+                    System.out.println("Qual o nome que deseja editar?");
+                    String nameToEdit = scanner.next();
+                    System.out.println("Qual é o novo nome?");
+                    String newName = scanner.next();
+                    editPessoa(pessoas, nameToEdit,newName);
+                    break;
+                case 5 :
+                    LocalDate dataAtual = LocalDate.now();
+                    int anoAtual = dataAtual.getYear();
+                    int mesAtual = dataAtual.getMonthValue();
+                    idadePessoa(pessoas, anoAtual,mesAtual);
+                    break;
+                case 6 :
+                    sexoPessoa(pessoas);
+                    break;
+                case 7 :
                     exit = false;
                     break;
                 default:
@@ -47,13 +62,15 @@ public class Main {
         }
     }
 
-    private static void addPessoas(List<Person> pessoas){
+    public static void addPessoas(List<Person> pessoas){
         Person pessoa = new Person();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite o nome completo da pessoa");
         pessoa.name = scanner.nextLine();
-        System.out.println("Digite a Data de Nascimento da pessoa");
+        System.out.println("Digite o sexo da pessoa");
+        pessoa.sexo = scanner.nextLine();
+        System.out.println("Digite a Data de Nascimento da pessoa. Ex(01/01/2001)");
         pessoa.dtNas = scanner.nextLine();
         System.out.println("Digite o endereço da pessoa");
         pessoa.endereco = scanner.nextLine();
@@ -61,32 +78,88 @@ public class Main {
         System.out.println("Obrigado por digitar o nome completo do " + pessoa.name);
     }
 
-    private static void findPessoa(List<Person> pessoas, String pessoaName) {
-
-        for(int count = 0; count < pessoas.size(); count++){
+    public static void findPessoa(List<Person> pessoas, String pessoaName) {
+        String nameFound="";
+        String dataFound="";
+        String addressFound="";
+        for(int count = 0; count < pessoas.size(); count++) {
             Person pessoa = pessoas.get(count);
-            if(pessoa.name.equals(pessoaName)){
-                System.out.println("Encontrei o " + pessoaName + "! Ele nasceu em " + pessoa.dtNas + " e mora no endereço:"
-                + pessoa.endereco + "!");
-            } else {
-                System.out.println("A pessoa " + pessoaName + " não possui cadastro!"); // Essa validação tbm não deu certo
+            if (pessoa.name.equals(pessoaName)) {
+                nameFound=pessoaName;
+                dataFound=pessoa.dtNas;
+                addressFound=pessoa.endereco;
             }
+        }
+        if (nameFound!="") {
+            System.out.println("Encontrei o " + nameFound + "! Ele nasceu em " + dataFound + " e mora no endereço:"
+                    + addressFound + "!");
+        } else {
+            System.out.println("A pessoa " + nameFound + " não possui cadastro!"); // Essa validação tbm não deu certo
         }
     }
     private static void removePessoa(List<Person> pessoas, String pessoaName) {
-
-        for(int count = 0; count < pessoas.size(); count++){
+        String nameFound="";
+        for(int count = 0; count < pessoas.size(); count++) {
             Person pessoa = pessoas.remove(count);
-            if(pessoa.name.equals(pessoaName)){
-                System.out.println("Pessoa " + pessoaName + ", removida com sucesso!");
+            if (pessoa.name.equals(pessoaName)) {
+                nameFound = pessoaName;
+                ;
             }
-            if(pessoa.name!=pessoaName){
-                System.out.println("A pessoa " + pessoaName + " não possui cadastro!"); //Tentei colocar algumas
-                // validações para dar uma mensagem caso o nome não existissem, mas não deu certo. Se puder dar um feed back
-                // as pesquisas na internet também não surtiu efeito.
-                //
-            }
-
         }
+        if(nameFound!=""){
+            System.out.println("Pessoa " + pessoaName + ", removida com sucesso!");
+        }else {
+            System.out.println("A pessoa " + pessoaName + " não possui cadastro!");
+        }
+    }
+    private static void editPessoa(List<Person> pessoas, String pessoaName,String newName){
+        String nameFound="";
+        for(int count = 0; count < pessoas.size(); count++){
+            Person pessoa = pessoas.get(count);
+            String nome = pessoa.name;
+            if(nome.equals(pessoaName)){
+                nameFound= newName;
+                pessoa.name = newName;
+                System.out.println("Pessoa " + pessoaName + ", editada com sucesso!");
+            }
+        }
+        if(nameFound!=""){
+            System.out.println("Pessoa " + pessoaName + ", editada para "+ newName + " com sucesso!");
+        }else {
+            System.out.println("Pessoa " + pessoaName + ",não encontrada!");
+        }
+    }
+    private static void idadePessoa(List<Person> pessoas, Integer anoAtual, Integer mesAtual){
+        String nameFound="";
+        for(int count = 0; count < pessoas.size(); count++) {
+            Person pessoa = pessoas.get(count);
+            String[] data = pessoa.dtNas.split("/");
+            String dia = data[0];
+            String mes = data[1];
+            String ano = data[2];
+            int month = Integer.parseInt(mes);
+            int year = Integer.parseInt(ano);
+            int age = anoAtual - year;
+            if(mesAtual<month){
+                pessoa.idade=age-1;
+                System.out.println("Pessoa " + pessoa.name + ", possui "+ pessoa.idade + " anos!");
+            }
+            else{
+                pessoa.idade=age;
+                System.out.println("Pessoa " + pessoa.name + ", possui "+ pessoa.idade + " anos!");
+            }
+        }
+    }
+    public static void sexoPessoa(List<Person> pessoas) {
+        int countM = 0;
+        int countF = 0;
+        for(int count = 0; count < pessoas.size(); count++){
+            if(pessoas.get(count).sexo.equals("Masculino")){
+                countM++;
+            }else{
+                countF++;
+            }
+        }
+        System.out.println("O cadastro possui: " + countM + " do sexo masculino e "+countF+" do sexo feminino!" );
     }
 }
--- Person.java
+++ Person.java
@@ -2,14 +2,21 @@ public class Person {
     public String name;
     public String dtNas;
     public String endereco;
+    public String sexo;
+    public Integer idade;
+
 
     public void setName(String name){
         this.name=name;
     }
-    public void setDTNas(String dtNas){
-        this.dtNas=dtNas;
-    }
+    public void setDTNas(String dtNas){this.dtNas=dtNas;}
     public void setEndereco(String endereco){
         this.endereco=endereco;
+    }
+    public void setSexo(String sexo){
+        this.sexo=sexo;
+    }
+    public void setIdade(Integer idade){
+        this.idade=idade;
     }
 }
